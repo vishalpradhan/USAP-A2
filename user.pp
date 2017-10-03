ddc9263a75749f8135d7205f13e2fdee3ac2ced5
Cloud User <ec2-user@ip-172-31-9-87.us-west-2.compute.internal>
2017-10-03 01:02:29 +0000

    ncluded user and packages

---
+++ user.pp
@@ -0,0 +1,59 @@
+class user {
+	include packages
+
+	group{ 'sysadmin':
+	ensure => present,
+	gid =>'500',
+
+	}
+
+	group{ 'cars':
+	ensure => present,
+	gid =>'501',
+	}
+
+	group{ 'trucks':
+	ensure => present,
+	gid =>'502',
+	}
+
+	group{ 'ambulances':
+	ensure => present,
+	gid =>'503',
+	}
+
+	 user { 'becca':
+        ensure => present,
+        home => '/home/becca',
+        managehome => true,
+        uid => '10010077',
+        groups => ['sysadmin', 'cars'],
+        shell => '/bin/bash',
+        }
+
+        user { 'fred':
+        ensure => present,
+        home => '/home/fred',
+        managehome => true,
+        uid => '10020077',
+        groups => ['trucks', 'cars'],
+        shell => '/bin/csh',
+        }
+
+        user { 'wilma':
+        ensure => present,
+        home => '/home/wilma',
+        managehome => true,
+        uid => '10030077',
+        groups => ['trucks', 'cars', 'ambulances'],
+        }
+
+	#ssh_auth_for_wilma
+	ssh_authorized_key{'wilma_ssh':
+	ensure => present,
+	user => 'wilma',
+	type => 'ssh-rsa',
+	key => 'WilmasKeyGeneratedHere',
+	
+	}
+}
