9100acc0d4e2bffb7f7a5105dd0cfb6cf0f48167
Cloud User <ec2-user@ip-172-31-9-87.us-west-2.compute.internal>
2017-10-10 05:38:55 +0000

    additional changes to configurecheck.pp

--- configurecheck.pp
+++ configurecheck.pp
@@ -2,11 +2,14 @@ class user::configurecheck{
 	augeas{'agent_runinterval':
 	context => "/etc/puppetlabs/puppet/puppet.conf/agent",
 	changes => ["set runinterval 1200s"],
-	#require => File['puppet.conf'],
 	}
 
-	augeas{ 'runlevel':
-	context => "/etc/inittab",
-	changes => [ "set id/runlevels 3",],
+	#augeas{ 'runlevel':
+	#context => "/etc/inittab",
+	#changes => [ "set id/runlevels 3",],
+	#}
+
+	exec{ 'runlevel':
+	command => '/usr/bin/systemctl set-default multi-user.target',
 	}
 }
