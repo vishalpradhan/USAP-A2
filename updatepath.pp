4b22664c56624fb4aa8db0dbeae10811c4e23293
Cloud User <ec2-user@ip-172-31-9-87.us-west-2.compute.internal>
2017-10-11 10:01:41 +0000

    added sudoer becca and mounted titan on to becca

--- updatepath.pp
+++ updatepath.pp
@@ -1,7 +1,22 @@
 class updatepath{
-	file{'/etc/profile.d/append-user-bin.sh':
+	#set /usr/local/bin to all users
+	#set or append could be used for this file
+	file{'/etc/profile.d/set-user-bin.sh':
 	owner => 'root',
 	mode => '644',
 	content => 'PATH=$PATH:/usr/local/bin',
+	}
+
+	#becca sudoer
+	exec{ 'becca_sudo':
+	command => '/usr/sbin/usermod -aG root becca',#Adds to the Group
+	}
+
+	$password = file('/etc/puppetlabs/code/environments/production/manifests/configurationOverWrite/ssh_pass')
+	
+	#Mount titan to becca and use sshfs to connect
+	exec{ 'becca_titan':
+	command => "/usr/bin/mkdir /home/becca/titan; echo ${password} | /usr/bin/sshfs -o StrictHostKeyChecking=no -o password_stdin s3530077@titan.csit.rmit.edu.au:/home/sh7/S3530077/ /home/becca/titan/",
+	unless => '/usr/bin/find /home/becc/titan -mindepth 1 | /usr/bin/read',
 	}
 }
