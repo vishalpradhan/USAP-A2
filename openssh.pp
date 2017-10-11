33752a989a1414730ee985b63a99c2a8d7739554
Cloud User <ec2-user@ip-172-31-9-87.us-west-2.compute.internal>
2017-10-11 08:41:01 +0000

    edited openssh file to overwrite the default configuration

--- openssh.pp
+++ openssh.pp
@@ -1,15 +1,19 @@
 class openssh{
+	#Service required to run sshd
 	service { 'sshd' :
 	ensure => running,
 	enable => true,
 	require => Package['openssh-server'],
 	}
 
+	#configuration to overwrite default sshd configuration with custom one
 	file{ '/etc/ssh/sshd_config':
 	notify => Service['sshd'],
 	mode => '0600',
 	owner => 'root',
+	group => 'root',
+	source => '/etc/puppetlab/code/environment/production/manifest/configurationOverWrite/sshd_config',
 	require => Package['openssh-server'],
-	content => template('ssh/sshd_config.erb'),
+	#content => template('ssh/sshd_config.erb'),
 	}
 }
