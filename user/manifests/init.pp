b2c03a0b29a19bf408bb30fefaa46772dbdd2437
Cloud User <ec2-user@ip-172-31-9-87.us-west-2.compute.internal>
2017-10-12 15:08:43 +0000

    puppet-lint style coding

---
+++ user/manifests/init.pp
@@ -0,0 +1,64 @@
+  #create users specified
+  class user {
+    require packages
+    #Creating different groups 	
+    group{ 'sysadmin':
+      ensure => present,
+      gid    =>'500',
+    }
+
+    group{ 'cars':
+      ensure => present,
+      gid    =>'501',
+    }
+
+    group{ 'trucks':
+      ensure => present,
+      gid    =>'502',
+    }
+
+    group{ 'ambulances':
+      ensure => present,
+      gid    =>'503',
+    }
+
+    #Creating three different users 
+    user { 'becca':
+      ensure     => present,
+      home       => '/home/becca',
+      managehome => true,
+      password   => '$1$1cIWPs7b$JxeyaJ0JLbUQ7avmrFyx51', #the password is "password"
+      uid        => '10010077',
+      groups     => ['sysadmin', 'cars'],
+      shell      => '/bin/bash',
+    }
+
+    user { 'fred':
+      ensure     => present,
+      home       => '/home/fred',
+      managehome => true,
+      password   => '$1$1cIWPs7b$JxeyaJ0JLbUQ7avmrFyx51', #the password is "password"
+      uid        => '10020077',
+      groups     => ['trucks', 'cars'],
+      shell      => '/bin/csh',
+    }
+
+    user { 'wilma':
+      ensure         => present,
+      home           => '/home/wilma',
+      managehome     => true,
+      password       => '$1$1cIWPs7b$JxeyaJ0JLbUQ7avmrFyx51', #the password is "password"
+      uid            => '10030077',
+      groups         => ['trucks', 'cars', 'ambulances'],
+      shell          => '/bin/bash',
+      purge_ssh_keys => true,
+    }
+
+    #ssh_auth_for_wilma
+    ssh_authorized_key{'wilma_ssh':
+      ensure => present,
+      user   => 'wilma',
+      type   => 'ssh-rsa',
+      key    => 'WilmasKeyGeneratedHere',
+    }
+  }
