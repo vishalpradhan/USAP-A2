c9eedb4dfb96f82b9adc3b35dcea25f37f60a199
Cloud User <ec2-user@ip-172-31-9-87.us-west-2.compute.internal>
2017-10-12 15:06:40 +0000

    puppet-lint style coding

---
+++ httpdconfig/manifests/init.pp
@@ -0,0 +1,24 @@
+  #This class over writes the default config file
+  class httpdconfig{
+    file{ '/var/www/s3530077':
+      ensure => 'directory',
+      owner  => 'root',
+      mode   => '0600',
+    }
+    #initial setup of the httpd config file
+    ->service{ 'httpd':
+      ensure => running,
+      enable => true,
+    }
+
+    #Over writing the default httpd.conf file with the custome one
+    ~>file{ '/etc/httpd/conf/httpd.conf':
+      ensure  => present,
+      #notify  => Service['httpd'],
+      mode    => '0777',
+      owner   => 'root',
+      group   => 'root',
+      source  => '/etc/puppetlabs/code/environments/production/manifests/configurationOverWrite/httpd.conf',
+      require => Package['httpd'],
+    }
+  }
