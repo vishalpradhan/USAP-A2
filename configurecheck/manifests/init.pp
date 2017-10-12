1284514c78a65afbb45f9210d98f1a398b376108
Cloud User <ec2-user@ip-172-31-9-87.us-west-2.compute.internal>
2017-10-12 15:05:34 +0000

    puppet-lint style coding

---
+++ configurecheck/manifests/init.pp
@@ -0,0 +1,18 @@
+  #set the runtime interval from default to 20 mins
+  class user::configurecheck{
+    #Set run interval to 20 minutes.
+    augeas{'agent_runinterval':
+      context => '/etc/puppetlabs/puppet/puppet.conf/agent',
+      changes => ['set runinterval 1200s'],
+    }
+
+    #augeas{ 'runlevel':
+    #context => "/etc/inittab",
+    #changes => [ "set id/runlevels 3",],
+    #}
+
+    #Ensures installed packages comes up on boot
+    exec{ 'runlevel':
+      command => '/usr/bin/systemctl set-default multi-user.target',
+    }
+  }
